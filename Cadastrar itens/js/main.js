9e0f30ed0f24e33164edd1dfdfd0f7b318c264df
GuilhermeNascimento36 <guilhermenarruda@gmail.com>
2022-12-18 19:20:43 -0300

    add items in local  storage

--- Cadastrar itens/js/main.js
+++ Cadastrar itens/js/main.js
@@ -1,27 +1,52 @@
 var fmrAdicionar = document.getElementById("add-item");
+var itens = [];
+
 
 fmrAdicionar.addEventListener("submit", (dados) => {
     dados.preventDefault(); //cancela o comportamento padrão para não ficar recarregando
 
     //estamos chamando o target do vetor, só que pelo nome do input
-    criarElemento(dados.target.elements['nome'].value, dados.target.elements['quantidade'].value);
+    let nome = dados.target.elements['nome'].value;
+    let quantidade = dados.target.elements['quantidade'].value;
+    criarElemento(nome, quantidade);
 });
 
 function criarElemento (nome, quantidade){
-    //<li class="item"><strong>3</strong>Torrada</li>
 
-    let novoItem = document.createElement("li");
-    novoItem.classList.add("item");
-
-    //anexando no html e na árvore com o appendChild
-    let qtdItem = document.createElement("strong");
-    qtdItem.innerHTML = quantidade;
-
-    novoItem.appendChild(qtdItem);
+    if(nome.length > 0 && quantidade.length > 0 && quantidade > 0){
+        let novoItem = document.createElement("li");
+        novoItem.classList.add("item");
     
-    //o nome vem dps da tag strong
-    novoItem.innerHTML += nome;
+        //anexando no html e na árvore com o appendChild
+        let qtdItem = document.createElement("strong");
+        qtdItem.innerHTML = quantidade;
+    
+        novoItem.appendChild(qtdItem);
+        
+        //o nome vem dps da tag strong
+        novoItem.innerHTML += nome;
+    
+        let listaItems = document.getElementById("lista-items");
+        listaItems.appendChild(novoItem);
 
-    let listaItems = document.getElementById("lista-items");
-    listaItems.appendChild(novoItem);
-}
+        //objeto referente ao item do array
+        let item = {
+            "nome": nome,
+            "quantidade": quantidade
+        }
+        
+        itens.push(item);
+
+        //salvando no localstorage do navegador e convertendo o objeto para tipo string (por conta do json)
+        localStorage.setItem("item", JSON.stringify(itens));
+    }
+
+    else if(quantidade < 0){
+        console.log("Não é possível obter uma quantidade negativa.");
+        }
+
+    else{
+        console.log("Erro! Não é possível adicionar um item nulo.");
+    }
+ }
+ 
