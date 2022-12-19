fd7a99455f2861f249f694a013e73c7c3557693a
GuilhermeNascimento36 <guilhermenarruda@gmail.com>
2022-12-19 13:56:11 -0300

    atualizando quantidade no local storage

--- Cadastrar itens/js/main.js
+++ Cadastrar itens/js/main.js
@@ -21,27 +21,41 @@ fmrAdicionar.addEventListener("submit", (dados) => {
         "quantidade": quantidade.value
     }
 
-    if(quantidade.value.length > 0 && nome.value.length > 0 && quantidade.value > 0){
-        criarElemento(item);
-        itens.push(item);
-        
-        //salvando no localstorage do navegador e convertendo o objeto para tipo string (por conta do json)
-        localStorage.setItem("itens", JSON.stringify(itens));   
-    }
+    let verificaElemento = itens.find(elemento => elemento.nome === nome.value);
 
-    else if(quantidade.value < 0){
-        console.log("Não é possível obter uma quantidade negativa.");
-        alert("Erro! Não é possível adicionar uma quantidade negativa.");
-    }
+    if(verificaElemento){
+        //elemento encontrado, alocamos o valor no elemento de mesmo id
+        item.id = verificaElemento.id;
+        atualizarItens(item);
 
+        //atualizando no array
+        itens[verificaElemento.id] = item;
+    }
     else{
-        console.log("Erro! Não é possível adicionar um item nulo.");
-        alert("Erro! Não é possível adicionar um produto nulo.");
+        if(quantidade.value.length > 0 && nome.value.length > 0 && quantidade.value > 0){
+            item.id = itens.length;
+            criarElemento(item);
+            itens.push(item); 
+        }
+    
+        else if(quantidade.value < 0){
+            console.log("Não é possível obter uma quantidade negativa.");
+            alert("Erro! Não é possível adicionar uma quantidade negativa.");
+        }
+    
+        else{
+            console.log("Erro! Não é possível adicionar um item nulo.");
+            alert("Erro! Não é possível adicionar um produto nulo.");
+        }
     }
+
+    //salvando no localstorage do navegador e convertendo o objeto para tipo string (por conta do json)
+    localStorage.setItem("itens", JSON.stringify(itens));  
 
     nome.value = "";
     quantidade.value = "";
 });
+
 
 function criarElemento (item){    
     let novoItem = document.createElement("li");
@@ -51,10 +65,18 @@ function criarElemento (item){
     let qtdItem = document.createElement("strong");
     qtdItem.innerHTML = item.quantidade;
 
+    //alocando id no item com data atribute
+    qtdItem.dataset.id = item.id;
+
     novoItem.appendChild(qtdItem);
     
     //o nome vem dps da tag strong
     novoItem.innerHTML += item.nome;
 
     listaItems.appendChild(novoItem);
+}
+
+function atualizarItens(item){
+    //adicionando a quantidade de acordo com o data-id passado
+    document.querySelector("[data-id='" + item.id + "']").innerHTML = item.quantidade;
 }
